89180fd4fa6f969f06ba92bf19945d41623fab5f
Glenn Hamilton <g.hamilton@kainos.com>
2023-09-22 13:29:17 +0100

    Accessibility Test

---
+++ .pa11yci.conf.js
@@ -0,0 +1,24 @@
+const defaults = {
+    standard: 'WCAG2AA',
+    timeout: 50000,
+    runners: ['axe']
+};
+
+const urls = [
+    '${URL}'
+];
+
+function myPa11yCiConfiguration() {
+    console.error('Env:', process.env.URL)
+
+    for (let idx = 0; idx < urls.length; idx++) {
+        urls[idx] = urls[idx].replace('${URL}', process.env.URL)
+    }
+
+    return {
+        defaults: defaults,
+        urls: urls,
+    }
+}
+
+module.exports = myPa11yCiConfiguration()
